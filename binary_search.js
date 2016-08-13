fa2219aa9e43836cc2ac4a91cd3e4640ba660ac1
Enakshi Shah <enakshi.shah@mail.utoronto.ca>
2016-08-13 14:12:28 -0400

    binary search without recursion added

--- binary_search.js
+++ binary_search.js
@@ -1,5 +1,5 @@
 
-
+// recursive
 function binarySearch(array, i){
 	var sorted = array.sort()
 	var length = sorted.length
@@ -22,3 +22,27 @@ function binarySearch(array, i){
 
 var array = [1, 4, 2, 2, 4, 6, 8, 3, 9, 5, 1];
 binarySearch(array, 12);
+
+// without recursion
+
+function binary(array, i){
+	var low = 0;
+	var high = array.length;
+	while (low < high) {
+		var mid = Math.floor((low + high)/2);
+		if (array[mid] == i){
+			console.log("Found: " + i + " at index: " + mid)
+			break;
+		} else if (array[mid] > i){
+			high = mid - 1;
+		} else if (array[mid] < i){
+			low = mid + 1;
+		} else {
+			console.log("Not found")
+			return -1;
+		}
+	} //end while
+} //end func
+
+var array = [1, 4, 2, 2, 4, 6, 8, 3, 9, 5, 1];
+binary(array, 6);
